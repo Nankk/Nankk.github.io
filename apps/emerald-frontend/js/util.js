3382e8fdce249193c4678cfc4b6fc872d2d1a7e1
Nankk Home <toc32768@yahoo.co.jp>
2021-03-15 22:02:10 +0900

    Deploy emerald-frontend

---
+++ apps/emerald-frontend/js/util.js
@@ -0,0 +1,9 @@
+function toDataURLPromise(file){
+    return new Promise((resolve, reject) => {
+        var fr = new FileReader();
+        fr.onload = () => {
+            resolve(fr.result);
+        };
+        fr.readAsDataURL(file);
+    });
+}
